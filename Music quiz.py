02631cac98965013a66f564e993f00d2ef23018d
nyphro <silviuhostinariumihai@gmail.com>
2025-04-03 15:01:16 +0100

    Add files via upload

---
+++ Music quiz.py
@@ -0,0 +1,30 @@
+
+#███╗░░░███╗░█████╗░██████╗░███████╗  ██████╗░██╗░░░██╗  ░██████╗██╗██╗░░░░░██╗░░░██╗██╗██╗░░░██╗
+#████╗░████║██╔══██╗██╔══██╗██╔════╝  ██╔══██╗╚██╗░██╔╝  ██╔════╝██║██║░░░░░██║░░░██║██║██║░░░██║
+#██╔████╔██║███████║██║░░██║█████╗░░  ██████╦╝░╚████╔╝░  ╚█████╗░██║██║░░░░░╚██╗░██╔╝██║██║░░░██║
+#██║╚██╔╝██║██╔══██║██║░░██║██╔══╝░░  ██╔══██╗░░╚██╔╝░░  ░╚═══██╗██║██║░░░░░░╚████╔╝░██║██║░░░██║
+#██║░╚═╝░██║██║░░██║██████╔╝███████╗  ██████╦╝░░░██║░░░  ██████╔╝██║███████╗░░╚██╔╝░░██║╚██████╔╝
+#╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝  ╚═════╝░░░░╚═╝░░░  ╚═════╝░╚═╝╚══════╝░░░╚═╝░░░╚═╝░╚═════╝░
+
+username = None
+password = None
+
+def auth():#check if user is allowed to play game
+    username = input("enter username")
+    password = input("enter password")
+
+    if username == "Silviu":
+        if password == "Password":
+            return print("Welcome to the Music Quiz!")
+    elif username == "Nyphro":
+        if password == "Password":
+            return print("Welcome to the Music Quiz!")
+    else:
+        print("Access denied!")
+
+auth() #Authenticate that user can play game
+
+## Part 1 ^^^^^^^^^
+
+
+
